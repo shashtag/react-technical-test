326aca7f2b61adc25c1319cce606ef17ea2f4c54
Shashwat Gupta <shashwatsatna@gmail.com>
2023-08-15 21:49:35 +0530

    get data

---
+++ src/Data/data.js
@@ -0,0 +1,55 @@
+import AlexaPopen from "../../public/images/Alexa_Popen.png";
+import BrittanWheeler from "../../public/images/Brittan_Wheeler.png";
+import LeslieAlexander from "../../public/images/Leslie_Alexander.png";
+
+const data = [
+  {
+    stars: 4,
+    comment: "You made it so simp",
+    img: BrittanWheeler,
+    name: "Brittan Wheeler",
+    designation: "Consultant",
+  },
+  {
+    stars: 3,
+    comment:
+      "You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.",
+    img: AlexaPopen,
+    name: "Alexa Popen",
+    designation: "CEO",
+  },
+  {
+    stars: 5,
+    comment:
+      "“You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.”",
+    img: LeslieAlexander,
+    name: "Leslie Alexander",
+    designation: "Founder",
+  },
+  {
+    stars: 5,
+    comment:
+      "“You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.”",
+    img: LeslieAlexander,
+    name: "Leslie Alexander",
+    designation: "Founder",
+  },
+  {
+    stars: 5,
+    comment:
+      "“You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.”",
+    img: LeslieAlexander,
+    name: "Leslie Alexander",
+    designation: "Founder",
+  },
+  {
+    stars: 5,
+    comment:
+      "“You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.”",
+    img: LeslieAlexander,
+    name: "Leslie Alexander",
+    designation: "Founder",
+  },
+];
+
+export default data;
