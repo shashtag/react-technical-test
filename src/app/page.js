5642e185e4b10cc200ec5f447c689f7dbfcd8fb4
Shashwat Gupta <shashwatsatna@gmail.com>
2023-08-15 23:53:09 +0530

    container

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Griffy } from "next/font/google";
+import ab from "../../public/icons/Full_Star.svg";
+import Card from "@/Components/Card";
+import cardData from "@/Data/cardData";
 
 export default function ReviewPage() {
   return (
-    <>
-        Hello World
-    </>
-  )
+    <div className='hero '>
+      {cardData.map((item) => (
+        <Card key={item.name} />
+      ))}
+    </div>
+  );
 }
